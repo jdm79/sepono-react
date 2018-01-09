2791c40a22535a2af7ec09ec872427a42b0b69b7
George <george.cook@redant.com>
2018-01-09 22:29:57 +0000

    Add react router with an example of nested routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+
+import Home from './Home'
+import About from './About'
+import User from './User'
+
 import './App.css'
 
-const App = () => <div className='app'><h1>Hello World!</h1></div>
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/user'>User</Link></li>
+      </ul>
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/user' component={User} />
+    </div>
+  </Router>
+)
 
 export default App
